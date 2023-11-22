63fde611449949ca45c795d4053a80b19dcd4803
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-11-22 17:14:21 +0200

    Play with Routes

---
+++ routes/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/admin', function () {
+    return 'Admin page';
+});
+
+// Combine many class into namespace
+Route::namespace('App\Http\Controllers')->group(function(){
+
+// set prefix for set of routes
+    Route::prefix('users')->group(function(){
+        Route::get('/show','UserController@showAdminName');
+    });
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// parameter
+
+// required
+Route::get('/test/{id}', function ($id) {
+    return $id ;
+});
+
+// NOT required
+Route::get('/test1/{id?}', function () {
+    return 'Not required parameter' ;
+});
+
+// route name
+Route::get('/test/', function () {
+    return 'route name' ;
+})->name('route-name');
+
+
