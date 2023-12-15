8a66ae23c956930346d7cd103cabef28c02611f2
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-12-15 03:01:35 +0200

    PLay with Form Request ans Rules validation

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TestCreate;
 use Illuminate\Http\Request;
 use App\Models\Test;
 use Illuminate\Support\Facades\DB;
@@ -31,21 +32,22 @@ class ExampleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TestCreate $request)
     {
-        $data = $request->validate([
-            'name' => 'required|string',
-            'content' => 'required|string',
-            'status'=>'required',
-            'show'=>'required'
-        ]);
-        
-        Test::create($data);
+        // $data = $request->validate([
+        //     'name' => 'required|string',
+        //     'content' => 'required|string',
+        //     'status'=>'required',
+        //     'show'=>'required'
+        // ]);
+
+        // echo $request->validated();
+        Test::create($request->validated());
         // $test=new Test;
         // $test->name= $request->name;
         // $test->content=$request->content;
         // $test->save();
-        return 'This is store Route : '.$request->name.'    '.request('content');
+        return 'Done';
     }
 
     /**
